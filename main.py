1148958ee67428dd273ffd2b0f7d52f561fd664d
Jopacabra <94020329+Jopacabra@users.noreply.github.com>
2022-02-21 14:15:28 -0700

    Adjusted Result Saving Behavior and Added Config File & Scanning Modes
    - New config file to select options
    - Saving adjustable with hard-coded frequency
    - New operation modes (scan each background and / or randomize params)

--- main.py
+++ main.py
@@ -3,6 +3,48 @@ import numpy as np
 import pandas as pd
 import grid_reader as gr
 import medium_integral as mi
+import yaml
+
+# Read config file and parse settings
+with open('config.yml', 'r') as ymlfile:
+    # Note the usage of yaml.safe_load()
+    # Using yaml.load() exposes the system to running any Python commands in the config file.
+    # That is unnecessary risk!!!
+    cfg = yaml.safe_load(ymlfile)
+
+# Set scanning modes
+RANDOM_EVENT = bool(cfg['mode']['RANDOM_EVENT'])  # Determines if we select a random event or iterate through each
+RANDOM_ANGLE = bool(cfg['mode']['RANDOM_ANGLE'])  # Determines if we select a random angle or do a complete sweep
+VARY_POINT = bool(cfg['mode']['VARY_POINT'])  # Determines if we vary the prod point or set it to (0,0)
+WEIGHT_POINT = bool(cfg['mode'][
+                        'WEIGHT_POINT'])  # NON-FUNCTIONAL - Determines if we weight point selection by T^6 in event - Needs VARYPOINT.
+
+# Set jet parameters
+N = int(cfg['jet_parameters']['NUM_JETS'])  # Number of jets to produce
+JET_ENERGY = int(
+    cfg['jet_parameters']['JET_ENERGY'])  # Jet energy - NON-FUNCTIONAL - Currently set in medium_integral.py
+
+# Set moment parameters
+K = int(cfg['moment_parameters']['MOMENT_K'])  # Which k-moment to calculate
+
+# Set config settings
+
+if RANDOM_ANGLE == False and VARY_POINT == False and RANDOM_EVENT == False:
+    print('Calculating angular sweep of moments for central jets in each background...')
+elif RANDOM_ANGLE == False and VARY_POINT == False and RANDOM_EVENT == True:
+    print('Calculating angular sweep of moments for central jets in random backgrounds...')
+elif RANDOM_ANGLE == True and VARY_POINT == True:
+    print('Calculating a given number of random jets at random angles in random backgrounds from library...')
+elif RANDOM_ANGLE == True and VARY_POINT == False:
+    print('Calculating a given number of central jets at random angles in random backgrounds from library...')
+elif RANDOM_ANGLE == False and VARY_POINT == True:
+    print('Calculating angular sweep of random jets in random backgrounds from library...')
+else:
+    print('Input invalid mode!!!!!')
+    print('RANDOM_ANGLE = ' + str(RANDOM_ANGLE))
+    print('VARY_POINT = ' + str(VARY_POINT))
+    print('RANDOM_EVENT = ' + str(RANDOM_EVENT))
+    print('WEIGHT_POINT = ' + str(WEIGHT_POINT))
 
 # Build array of interpolated backgrounds
 
@@ -18,7 +60,6 @@ folder_list = os.listdir(path='backgrounds/')
 # Append callable interpolating function to array of functions
 i = 0
 for file in folder_list:
-
     # read the grid file
     print('Reading grid data ... event ' + str(i))
     grid_data, grid_width, NT = gr.load_grid_file(file)
@@ -41,83 +82,164 @@ for file in folder_list:
 NUM_BACKGROUNDS = len(temp_func_array)
 print("Number of Backgrounds: " + str(NUM_BACKGROUNDS))
 
+
+def CalcMoments(event_num, X0, Y0, theta0):
+    # Calculate moment
+    moment = mi.moment_integral(temp_func_array[event_num], vel_x_func_array[event_num],
+                                vel_y_func_array[event_num], X0, Y0, theta0, K)
+    return moment
+
+
+
 # Call particular temp function with:
 # tempFuncArray[EVENT](np.array([TIME,XPOS,YPOS]))
 # Here TIME, XPOS, and YPOS are in fm.
 
 # Initialize result pandas dataframe
-results = pd.DataFrame(
+emptyFrame = pd.DataFrame(
+    {
+        "JET_ID": [],
+        "event_num": [],
+        "X0": [],
+        "Y0": [],
+        "theta0": [],
+        "pT_moment": [],
+        "pT_moment_error": [],
+    }
+)
+
+results = emptyFrame
+
+
+#
+def WriteDataLine(ID, event_num, X0, Y0, theta0, moment, momentErr):
+    dataframe = pd.DataFrame(
         {
-            "JET_ID": [],
-            "event_num": [],
-            "X0": [],
-            "Y0": [],
-            "theta0": [],
-            "pT_moment": [],
-            "pT_moment_error": [],
+            "JET_ID": [ID],
+            "event_num": [event_num],
+            "X0": [X0],
+            "Y0": [Y0],
+            "theta0": [theta0],
+            "pT_moment": [moment],
+            "pT_moment_error": [momentErr],
         }
     )
+    return dataframe
 
-# Set moment and number of jets
-N = 32  # Number of jets to produce
-K = int(0)  # Which moment to calculate
 
 # Preps file information and creates result file
 result_id = np.random.randint(0, 999999)
 print("Result ID number: " + str(result_id))
 results.to_csv('results/results' + str(result_id) + '.csv', mode='a', index=False, header=True)
 
-# Randomly choose N jet samples
-# Each sample has: event_num, X0, Y0, theta0
-# Adds each result to array of moments, with associated arrays of input parameters
-print("Calculating moments for " + str(N) + ' jets...')
-for ID in range(N):
-    print("Calculating moment for jet " + str(ID) + '...')
-    # Pick event
-    event_num = np.random.randint(0, NUM_BACKGROUNDS)  # random integer on [0,numBackgrounds)
+#################
+# Main Jet Loop #
+#################
+"""
+Choose jets and backgrounds according to user specified mode (from config.yml file)
 
-    # Generate random initial conditions
-    X0 = 0  # np.random.uniform(-1, 1)
-    Y0 = 0  # np.random.uniform(-1, 1)
-    theta0 = (2*np.pi/(N))*(ID + 1)  # np.random.uniform(0, 2 * np.pi)
+Each sample has: event_num, X0, Y0, theta0 and resulting pT_moment and pT_moment_error
+Error is as of yet pretty useless. Getting huge numbers out of integration.
 
-    # Calculate moment
-    moment = mi.moment_integral(temp_func_array[event_num], vel_x_func_array[event_num],
-                                vel_y_func_array[event_num], X0, Y0, theta0, K)
+Adds each result to pandas dataframe of moments with associated input parameters
+Saves results to csv every few jets and dumps active memory of the results.
+"""
+SAVE_EVERY = 10  # Set how many jets to calculate before each dump to file.
 
-    # Write data to a new dataframe
-    currentResults = pd.DataFrame(
-            {
-                "JET_ID": [ID],
-                "event_num": [event_num],
-                "X0": [X0],
-                "Y0": [Y0],
-                "theta0": [theta0],
-                "pT_moment": [moment[0]],
-                "pT_moment_error": [moment[1]],
-            }
-        )
+# Operation mode for scanning each event. Old and deprecated, but it works if you wanna run a test case.
+if RANDOM_EVENT == False:
+    for event_num in range(NUM_BACKGROUNDS):
+        for ID in range(N):
+            print("Calculating moment for jet " + str(ID) + ' for event ' + str(event_num) + '...')
+            # All events cycled through with complete angular profile.
+            # event_num = np.random.randint(0, NUM_BACKGROUNDS)  # random integer on [0,numBackgrounds)
 
-    # Append current result step to dataframe
-    results = results.append(currentResults)
+            # Generate random initial conditions
+            X0 = 0  # np.random.uniform(-1, 1)
+            Y0 = 0  # np.random.uniform(-1, 1)
+            theta0 = (2 * np.pi / (N)) * (ID + 1)  # np.random.uniform(0, 2 * np.pi)
 
-    if (ID != 0 and ID % 100 == 0):
+            # Calculate moment
+            moment = mi.moment_integral(temp_func_array[event_num], vel_x_func_array[event_num],
+                                        vel_y_func_array[event_num], X0, Y0, theta0, K)
+
+            # Write data to a new dataframe
+            currentResults = WriteDataLine(ID, event_num, X0, Y0, theta0, moment[0], moment[1])
+
+            # Append current result step to dataframe
+            results = results.append(currentResults)
+
+            # Write to csv
+            print('Saving progress...')
+            results.to_csv('results/results' + str(result_id) + '.csv', mode='a', index=False, header=False)
+
+            # Recreate empty results dataframe for next jet
+            column_names = [x for x in results.columns]
+            results = pd.DataFrame(columns=column_names)
+            # Print completion Confirmation
+            print('Jet ' + str(ID) + ' Complete')
+
+# Operation mode for random event selection.
+elif RANDOM_EVENT == True:
+    for ID in range(N):
+
+        # Randomly select event
+        event_num = np.random.randint(0, NUM_BACKGROUNDS)  # random integer on [0,numBackgrounds)
+
+        # Select jet production point
+        TARGETRADIUS = 1
+        if VARY_POINT == True and WEIGHT_POINT == False:
+            X0 = np.random.uniform(-TARGETRADIUS, TARGETRADIUS)
+            Y0 = np.random.uniform(-TARGETRADIUS, TARGETRADIUS)
+        elif VARY_POINT == True and WEIGHT_POINT == True:
+            # BROKEN!!!
+            X0 = np.random.uniform(-TARGETRADIUS, TARGETRADIUS)
+            Y0 = np.random.uniform(-TARGETRADIUS, TARGETRADIUS)
+        elif VARY_POINT == False:
+            X0 = 0
+            Y0 = 0
+        else:
+            print("Configuration error. All jet prod points = (0,0).")
+            X0 = 0
+            Y0 = 0
+
+        # Select jet production angle
+        if RANDOM_ANGLE == True:
+            theta0 = np.random.uniform(0, 2 * np.pi)
+        elif RANDOM_ANGLE == False:
+            theta0 = (2 * np.pi / (N)) * (ID + 1)  # np.random.uniform(0, 2 * np.pi)
+        else:
+            print("Configuration error. All jet angles = 0.")
+            theta0 = 0
+
+        # Calculate moment
+        moment = CalcMoments(event_num, X0, Y0, theta0)
+
+        # Write data to a new dataframe
+        currentResults = WriteDataLine(ID, event_num, X0, Y0, theta0, moment[0], moment[1])
+
+        # Append current result step to dataframe
+        results = results.append(currentResults)
+
+        # Declare jet complete
+        print('Jet ' + str(ID) + ' Complete')
+
         # Write to csv
-        print('Saving progress...')
-        results.to_csv('results/results' + str(result_id) + '.csv', mode='a', index=False, header=False)
+        if ID % SAVE_EVERY == 0 or ID == N-1:
 
-        # Recreate empty results dataframe
-        column_names = [x for x in results.columns]
-        results = pd.DataFrame(columns=column_names)
-    elif ID == N - 1:
-        # Write to csv
-        print('Saving results...')
-        results.to_csv('results/results' + str(result_id) + '.csv', mode='a', index=False, header=False)
-    else:
-        pass
+            # Save jets in results memory
+            print('Saving progress...')
+            results.to_csv('results/results' + str(result_id) + '.csv', mode='a', index=False, header=False)
 
-    print('Jet ' + str(ID) + ' Complete')
+            # Clear results memory
+            # Recreate empty results dataframe for next jet
+            column_names = [x for x in results.columns]
+            results = pd.DataFrame(columns=column_names)
 
+        # Declare if all jets complete
+        if ID == N - 1:
+            # Print completion Confirmation
+            print(str(N) + ' Jets Completed')
 
 # Remind user of ID and quit
 print("Results saved with ID number: " + str(result_id))

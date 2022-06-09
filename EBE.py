36c527409439b4c2149a258f2fc8f936ba83e5ad
Jopacabra <94020329+Jopacabra@users.noreply.github.com>
2022-06-09 17:08:56 -0500

    Trento pass to freestream fixes

--- EBE.py
+++ EBE.py
@@ -78,7 +78,7 @@ for eventNo in range(0, config.NUM_EVENTS):
 
     # Format trento data into initial conditions for freestream
     print('Packaging trento initial conditions into array...')
-    hic.toFsIc(initial_file='initial.hdf', quiet=False)
+    ic = hic.toFsIc(initial_file='initial.hdf', quiet=False)
 
     # Log randomseed and Trento event params
 

9adf7b344335880fd89c05cb5090fd4816dcc11a
Jopacabra <joseph.bahder@gmail.com>
2023-03-10 12:11:47 -0800

    Moved results pkl to /srv for transfer to OSG Connect login node.

--- osg/EBE.sh
+++ osg/EBE.sh
@@ -5,10 +5,21 @@ printf "Job is running on node: "; /bin/hostname
 printf "Job running as user: "; /usr/bin/id
 printf "Job is running in directory: "; /bin/pwd  # Should be /srv by default
 echo
-echo "Working hard..."
-# Activate the python virtual environment
+
+# Export osu-hydro and trento binary location to path
 export PATH=/usr/bin:$PATH
+
+# Activate the python virtual environment
+echo "Activating python virtual environment..."
 source /usr/jma/bin/activate
+
 # Run the script
+echo "Running event..."
 python3 /usr/jm-analysis/EBE.py
 echo "Event complete!"
+
+# Transfer relevant output files to /srv
+echo "Moving results pkl to /srv..."
+mv /usr/jm-analysis/results/*/*.pkl /srv
+
+echo "Job complete! Have a great day! :)"

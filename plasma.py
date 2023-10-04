Implemented flow gradient interpolation in plasma.py and fixed flowgrad effects in plasma_interaction.py.

plasma.py
@@ -139,26 +139,48 @@ class osu_hydro_file:
         return y_vel_data
 
     # Method to get raw flow x-direction gradient data
-    def flow_grad_x_array(self):
+    def grad_x_u_x_array(self):
         # Get the temp data as an array organized to be [time, x, y]-ish
         flow_x_data = self.x_vel_array()
 
         # Compute temperature gradient in the x-direction
-        flow_grad_x = np.gradient(flow_x_data, self.gridstep, axis=1)
+        grad_x_u_x = np.gradient(flow_x_data, self.gridstep, axis=1)
 
         # Spit out the array, organized the same as the temp_data array
-        return flow_grad_x
+        return grad_x_u_x
 
-    # Method to get raw flow y-direction gradient data
-    def flow_grad_y_array(self):
+    # Method to get raw flow x-direction gradient data
+    def grad_x_u_y_array(self):
         # Get the temp data as an array organized to be [time, x, y]-ish
         flow_y_data = self.y_vel_array()
 
-        # Compute temperature gradient in the y-direction
-        flow_grad_y = np.gradient(flow_y_data, self.gridstep, axis=2)
+        # Compute temperature gradient in the x-direction
+        grad_x_u_y = np.gradient(flow_y_data, self.gridstep, axis=1)
 
         # Spit out the array, organized the same as the temp_data array
-        return flow_grad_y
+        return grad_x_u_y
+
+    # Method to get raw flow x-direction gradient data
+    def grad_y_u_x_array(self):
+        # Get the temp data as an array organized to be [time, x, y]-ish
+        flow_x_data = self.x_vel_array()
+
+        # Compute temperature gradient in the x-direction
+        grad_y_u_x = np.gradient(flow_x_data, self.gridstep, axis=2)
+
+        # Spit out the array, organized the same as the temp_data array
+        return grad_y_u_x
+
+    # Method to get raw flow x-direction gradient data
+    def grad_y_u_y_array(self):
+        # Get the temp data as an array organized to be [time, x, y]-ish
+        flow_y_data = self.y_vel_array()
+
+        # Compute temperature gradient in the x-direction
+        grad_y_u_y = np.gradient(flow_y_data, self.gridstep, axis=2)
+
+        # Spit out the array, organized the same as the temp_data array
+        return grad_y_u_y
 
     # Method to plot raw temp data
     def plot_temps(self, time):
@@ -216,7 +238,7 @@ class osu_hydro_file:
     # Method to interpolate the x velocities from a grid file
     # Returns interpolating callable function
     def interpolate_x_vel_grid(self):
-        print('Interpolating x vel. grid data for event: ' + str(self.name))
+        print('Interpolating x-flow grid data for event: ' + str(self.name))
 
         # Cut x velocity data out and convert to numpy array
         x_vel_array = self.x_vel_array()
@@ -230,7 +252,7 @@ class osu_hydro_file:
     # Method to interpolate the y velocities from a grid file
     # Returns interpolating callable function
     def interpolate_y_vel_grid(self):
-        print('Interpolating y vel. grid data for event: ' + str(self.name))
+        print('Interpolating y-flow grid data for event: ' + str(self.name))
 
         # Cut y velocity data out and convert to numpy array
         y_vel_array = self.y_vel_array()
@@ -244,30 +266,58 @@ class osu_hydro_file:
     # Method to return interpolated function object from data
     # Function to interpolate the x direction flow gradient grid from the hydro file
     # Returns interpolating callable function
-    def interpolate_flow_grad_x_grid(self):
-        print('Interpolating flow x-gradient grid data for event: ' + str(self.name))
+    def interpolate_grad_x_u_x_grid(self):
+        print('Interpolating x-flow x-gradient grid data for event: ' + str(self.name))
 
         # Cut flow grad x data out and convert to numpy array
-        flow_grad_x_data = self.flow_grad_x_array()
+        grad_x_u_x_data = self.grad_x_u_x_array()
 
         # Interpolate data!
-        interp_flow_grad_x = RegularGridInterpolator((self.tspace, self.xspace, self.xspace), flow_grad_x_data)
+        interp_grad_x_u_x = RegularGridInterpolator((self.tspace, self.xspace, self.xspace), grad_x_u_x_data)
 
-        return interp_flow_grad_x
+        return interp_grad_x_u_x
 
     # Method to return interpolated function object from data
-    # Function to interpolate the temperature gradient grid from the hydro file
+    # Function to interpolate the x direction flow gradient grid from the hydro file
     # Returns interpolating callable function
-    def interpolate_flow_grad_y_grid(self):
-        print('Interpolating flow y-gradient grid data for event: ' + str(self.name))
+    def interpolate_grad_x_u_y_grid(self):
+        print('Interpolating y-flow x-gradient grid data for event: ' + str(self.name))
 
-        # Cut flow grad y data out and convert to numpy array
-        temp_grad_y_data = self.temp_grad_y_array()
+        # Cut flow grad x data out and convert to numpy array
+        grad_x_u_y_data = self.grad_x_u_y_array()
 
         # Interpolate data!
-        interp_flow_grad_y = RegularGridInterpolator((self.tspace, self.xspace, self.xspace), temp_grad_y_data)
+        interp_grad_x_u_y = RegularGridInterpolator((self.tspace, self.xspace, self.xspace), grad_x_u_y_data)
 
-        return interp_flow_grad_y
+        return interp_grad_x_u_y
+
+    # Method to return interpolated function object from data
+    # Function to interpolate the x direction flow gradient grid from the hydro file
+    # Returns interpolating callable function
+    def interpolate_grad_y_u_x_grid(self):
+        print('Interpolating x-flow y-gradient grid data for event: ' + str(self.name))
+
+        # Cut flow grad x data out and convert to numpy array
+        grad_y_u_x_data = self.grad_y_u_x_array()
+
+        # Interpolate data!
+        interp_grad_y_u_x = RegularGridInterpolator((self.tspace, self.xspace, self.xspace), grad_y_u_x_data)
+
+        return interp_grad_y_u_x
+
+    # Method to return interpolated function object from data
+    # Function to interpolate the x direction flow gradient grid from the hydro file
+    # Returns interpolating callable function
+    def interpolate_grad_y_u_y_grid(self):
+        print('Interpolating y-flow y-gradient grid data for event: ' + str(self.name))
+
+        # Cut flow grad x data out and convert to numpy array
+        grad_y_u_y_data = self.grad_y_u_y_array()
+
+        # Interpolate data!
+        interp_grad_y_u_y = RegularGridInterpolator((self.tspace, self.xspace, self.xspace), grad_y_u_y_data)
+
+        return interp_grad_y_u_y
 
     # Method to find the maximum temperature of a hydro file object
     def max_temp(self, time='i'):
@@ -301,7 +351,8 @@ class osu_hydro_file:
 # Plasma object as used for integration and muckery
 class plasma_event:
     def __init__(self, temp_func=None, x_vel_func=None, y_vel_func=None, grad_x_func=None, grad_y_func=None,
-                 flow_grad_x_func=None, flow_grad_y_func=None, event=None, name=None, rmax=None):
+                 grad_x_u_x_func=None, grad_x_u_y_func=None, grad_y_u_x_func=None, grad_y_u_y_func=None,
+                 event=None, name=None, rmax=None):
         # Initialize all the ordinary plasma parameters
         if event is not None:
             self.temp = event.interpolate_temp_grid()
@@ -309,8 +360,10 @@ class plasma_event:
             self.y_vel = event.interpolate_y_vel_grid()
             self.temp_grad_x = event.interpolate_temp_grad_x_grid()
             self.temp_grad_y = event.interpolate_temp_grad_y_grid()
-            self.flow_grad_x = event.interpolate_flow_grad_x_grid()
-            self.flow_grad_y = event.interpolate_flow_grad_y_grid()
+            self.grad_x_u_x = event.interpolate_grad_x_u_x_grid()
+            self.grad_x_u_y = event.interpolate_grad_x_u_y_grid()
+            self.grad_y_u_x = event.interpolate_grad_y_u_x_grid()
+            self.grad_y_u_y = event.interpolate_grad_y_u_y_grid()
             self.name = event.name
             self.timestep = event.timestep
             self.t0 = np.amin(self.temp.grid[0])
@@ -326,8 +379,10 @@ class plasma_event:
             self.y_vel = y_vel_func
             self.temp_grad_x = grad_x_func
             self.temp_grad_y = grad_y_func
-            self.flow_grad_x = flow_grad_x_func
-            self.flow_grad_y = flow_grad_y_func
+            self.grad_x_u_x = grad_x_u_x_func
+            self.grad_x_u_y = grad_x_u_y_func
+            self.grad_y_u_x = grad_y_u_x_func
+            self.grad_y_u_y = grad_y_u_y_func
             self.name = name
             try:
                 # Attempt to get timestep as if the functions are regular interpolator objects.

plasma_interaction.py
@@ -98,18 +98,25 @@ def flowgrad_drift_integrand(event, jet, time, tau):
     upar = event.u_par(point=jet_point, phi=jet_p_phi)
     ux = event.x_vel(jet_point)
     uy = event.y_vel(jet_point)
-    grad_temp_x = event.temp_grad_x(jet_point)
-    grad_temp_y = event.temp_grad_y(jet_point)
-    grad_flow_par = event.grad_par_flow(point=jet_point, phi=jet_p_phi)
-    grad_flow_perp = event.grad_perp_flow(point=jet_point, phi=jet_p_phi)
+    grad_x_temp = event.temp_grad_x(jet_point)
+    grad_y_temp = event.temp_grad_y(jet_point)
+    grad_perp_temp = np.cos(jet_p_phi + (np.pi/2))*grad_x_temp + np.sin(jet_p_phi + (np.pi/2))*grad_y_temp
+    grad_x_u_x = event.grad_x_u_x(jet_point)
+    grad_x_u_y = event.grad_x_u_y(jet_point)
+    grad_y_u_x = event.grad_y_u_x(jet_point)
+    grad_y_u_y = event.grad_y_u_y(jet_point)
+    grad_perp_u_tau = ( (np.cos(jet_p_phi + (np.pi/2))**2 )*grad_x_u_x + np.cos(jet_p_phi + (np.pi/2))*np.sin(jet_p_phi + (np.pi/2))*grad_x_u_y
+                      + np.cos(jet_p_phi + (np.pi/2))*np.sin(jet_p_phi + (np.pi/2))*grad_y_u_x + (np.sin(jet_p_phi + (np.pi/2))**2 )*grad_y_u_y)
+    grad_perp_u_perp = ( (np.cos(jet_p_phi + (np.pi/2))**2 )*grad_x_u_x + np.cos(jet_p_phi + (np.pi/2))*np.sin(jet_p_phi + (np.pi/2))*grad_x_u_y
+                      + np.cos(jet_p_phi + (np.pi/2))*np.sin(jet_p_phi + (np.pi/2))*grad_y_u_x + (np.sin(jet_p_phi + (np.pi/2))**2 )*grad_y_u_y)
     g = config.constants.G
     pt = jet.p_T()
     # Source link? -- Converts factor of fermi from integral to factor of GeV^{-1}
-    return - ((1 / FmGeV) * (g**2 / pt) * config.constants.K_FG_DRIFT * (3 / 2)  # * (((2*np.pi)**2)/2)  for new factor
+    return - ((1 / FmGeV) * (g**2 / pt) * config.constants.K_FG_DRIFT * (3 / 2)
         * tau * inv_lambda(event=event, jet=jet, point=jet_point)
-        * ((grad_temp_x * ux + grad_temp_y * uy) * (uperp/((1 - upar)**2)) * (3 * T * np.log(pt / (g * T)) - T )
-        + grad_flow_par * ((1/((1 - upar)**2)) + (2 * uperp/((1 - upar)**3))) * uperp * (T**2) * np.log(pt / (g * T))
-        * grad_flow_perp * (2 * uperp/((1 - upar)**2)) * (T**2) * np.log(pt / (g * T))))
+        * ((grad_perp_temp) * (uperp/((1 - upar)**2)) * (3 * T * np.log(pt / (g * T)) - T )
+        + grad_perp_u_tau * (2/((1 - upar)**3))  * uperp * (T**2) * np.log(pt / (g * T))
+        * grad_perp_u_perp * (2 * uperp/((1 - upar)**2)) * (T**2) * np.log(pt / (g * T))))
 
 # Function to sample ebe fluctuation zeta parameter for energy loss integral
 def zeta(q=0, maxAttempts=5, batch=1000):
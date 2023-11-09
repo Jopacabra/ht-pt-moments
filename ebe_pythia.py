4604bb0392cd614a62ee88fef6d9f739116fc6d8
Jopacabra <joseph.bahder@gmail.com>
2023-11-08 19:39:24 -0700

    Auto-convert datatypes for best memory utilization

--- ebe_pythia.py
+++ ebe_pythia.py
@@ -12,6 +12,36 @@ import timekeeper
 import pythia
 import ff
 
+
+# Function to downcast datatypes to minimum memory size for each column
+def downcast_numerics(df, verbose=True):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage(deep=True).sum() / 1024**2
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':  # for integers
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:  # for floats.
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+    end_mem = df.memory_usage(deep=True).sum() / 1024**2
+    if verbose: print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (start_mem - end_mem) / start_mem))
+    return df
+
 # Computes angular distance-ish quantity
 def delta_R(phi1, phi2, y1, y2):
     dphi = np.abs(phi1 - phi2)
@@ -44,9 +74,13 @@ def safe_exit(resultsDataFrame, hadrons_df, temp_dir, filename, identifier, keep
     logging.info('Saving progress...')
     logging.debug(resultsDataFrame)
     logging.debug(hadrons_df)
-    logging.info('printing dataframes before saving')
-    print(hadrons_df)
-    print(resultsDataFrame)
+    logging.info('Converting datatypes to reasonable formats...')
+    hadrons_df = hadrons_df.convert_dtypes()
+    resultsDataFrame = resultsDataFrame.convert_dtypes()
+    logging.info('Downcasting numeric values to minimum memory size...')
+    hadrons_df = downcast_numerics(hadrons_df)
+    resultsDataFrame = downcast_numerics(resultsDataFrame)
+    logging.info('Writing pickles...')
     resultsDataFrame.to_pickle(results_path + '/{}.pickle'.format(filename))  # Save dataframe to pickle
     hadrons_df.to_pickle(results_path + '/{}_hadrons.pickle'.format(filename))
 
